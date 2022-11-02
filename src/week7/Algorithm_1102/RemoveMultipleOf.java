b9691a8e38df546324883551edd79c30244b7980
byeonheungseob <kevin9160@naver.com>
2022-11-02 10:28:02 +0900

    에라토테네스의 채 리스트 2단계

--- src/week7/Algorithm_1102/RemoveMultipleOf.java
+++ src/week7/Algorithm_1102/RemoveMultipleOf.java
@@ -4,20 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RemoveMultipleOf {
-    public List<Integer> eratosthenes(int N) {
+    public int eratosthenes(int N) {
         List<Integer> list = new ArrayList<>();
         for (int i = 2; i <= N; i++) {
             list.add(i);
         }
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) % 2 == 0 && list.get(i) > 2) list.remove(i);
+        for (int j = 2; j*j< N ; j++) { //2의 배수 지우기 2제외
+            for(int i = 0; i < list.size(); i++){
+                if(list.get(i) % j == 0 && list.get(i) > j) list.remove(i);
+            }
         }
 
-        return list;
+
+
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i) % 2 == 0 && list.get(i) > 2) list.remove(i);
+//        }
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i) % 3 == 0 && list.get(i) > 3) list.remove(i);
+//        }
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i) % 4 == 0 && list.get(i) > 4) list.remove(i);
+//        }
+        return list.size();
     }
 
     public static void main(String[] args) {
+        RemoveMultipleOf rm = new RemoveMultipleOf();
+        System.out.println();
 
 
     }
